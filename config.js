d83268212f8784d66bb6a8698030d9360f0730db
AMILCAR OFICIAL <amilkarurquiagaramos1@gmail.com>
2026-08-15 07:15:29 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ export default {
   maxSubbots: 5,
   subbotsPorUsuario: 1,
   panelActivo: true,
-  panelPort: 3000,
+  panelPort: 3001,
   panelToken: 'AUTO_GENERATE',
   bienvenida: {
     activa: true,
