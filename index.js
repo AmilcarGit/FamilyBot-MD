dffdc9b54693c35f5f8d535c1f98a1dbc7a26c6e
AMILCAR OFICIAL <amilkarurquiagaramos1@gmail.com>
2026-08-21 07:40:15 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import * as baileysNS from '@whiskeysockets/baileys'
+const makeWASocket = baileysNS.default
 const {
   useMultiFileAuthState,
   DisconnectReason,
   makeCacheableSignalKeyStore,
   fetchLatestBaileysVersion,
-} = baileysNS.default || baileysNS
-const makeWASocket = baileysNS.makeWASocket || baileysNS.default?.makeWASocket || baileysNS.default || baileysNS
+} = baileysNS
 import { Boom } from '@hapi/boom'
 import pino from 'pino'
 import chalk from 'chalk'
@@ -107,7 +107,7 @@ async function iniciar() {
     generateHighQualityLinkPreview: true,
     syncFullHistory: false,
     getMessage: async (key) => {
-      return { conversation: config.nombreBot }
+      return { conversation: 'TheYui-MD' }
     }
   })
 
