f6782b2251952dbc0dc9ffc855a3d1c103fe578d
AMILCAR OFICIAL <amilkarurquiagaramos1@gmail.com>
2026-08-20 21:02:30 -0500

    Actualizar watchdog.sh

--- watchdog.sh
+++ watchdog.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-cd ~/TheYui-MD
+cd ~/FamilyBot-MD
 
 if pgrep -f "start.sh" > /dev/null; then
     exit 0
@@ -10,6 +10,6 @@ if ! pgrep -f "index.js" > /dev/null; then
     termux-wake-lock
     nohup bash start.sh > logs/watchdog.log 2>&1 &
     if command -v termux-notification > /dev/null; then
-        termux-notification --title "TheYui-MD" --content "El bot se habia caido, lo reinicie automaticamente." --priority high
+        termux-notification --title "FamilyBot-MD" --content "El bot se habia caido, lo reinicie automaticamente." --priority high
     fi
 fi
